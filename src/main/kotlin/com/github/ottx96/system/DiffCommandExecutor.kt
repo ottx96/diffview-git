f4b1829e9848cb2e0a9c1608c3b2751f87261262
Manuel Ott <manuel.ott@x.de>
2021-03-22 21:38:09 +0100

    Implemented DiffCommandExecutor.kt

--- src/main/kotlin/com/github/ottx96/system/DiffCommandExecutor.kt
+++
@@ -1,7 +0,0 @@
-package com.github.ottx96.system
-
-class DiffCommandExecutor() {
-
-
-
-}
---
+++ src/main/kotlin/com/github/ottx96/system/ShellCommandExecutor.kt
@@ -0,0 +1,33 @@
+package com.github.ottx96.system
+
+import com.github.ottx96.Entrypoint.Companion.verbose
+import com.github.ottx96.logging.Colors
+import com.github.ottx96.logging.Styles
+import java.io.File
+import java.util.concurrent.TimeUnit
+
+class ShellCommandExecutor(private val directory: File, private val file: File) {
+
+    private val err: File = File.createTempFile("DIFFVIEW.", ".TMP")
+    private val out: File = File.createTempFile("DIFFVIEW.", ".TMP")
+
+    fun execute(): String {
+        if(verbose) {
+            (Styles.ITALIC withColor Colors.WHITE).println("Starting directory: ${directory.absolutePath}")
+            (Styles.ITALIC withColor Colors.WHITE).println("Creating temporary file (err): ${err.absolutePath}")
+            (Styles.ITALIC withColor Colors.WHITE).println("Creating temporary file (out): ${out.absolutePath}")
+        }
+
+        val pb = ProcessBuilder().directory(directory.absoluteFile)
+            .redirectError(err).redirectOutput(out)
+            .command("git", "log", "-p", "--follow", file.absolutePath)
+
+        if(verbose) (Styles.ITALIC withColor Colors.WHITE).println("Running system command: ${pb.command().joinToString(separator = " ")}")
+
+            pb.start().waitFor(5, TimeUnit.SECONDS)
+
+        if(err.length() > 0) throw FileSystemException(err, reason = err.readText())
+        return out.readText()
+    }
+
+}
