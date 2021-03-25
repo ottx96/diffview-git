6438bd9747672cbe5166993b30a43890d2858fc9
Manuel Ott <manuel.ott@mait.de>
2021-03-25 18:18:07 +0100

    Implemented `git diff --no-index`

--- src/main/kotlin/com/github/ottx96/system/ShellCommandExecutor.kt
+++ src/main/kotlin/com/github/ottx96/system/ShellCommandExecutor.kt
@@ -1,18 +1,19 @@
 package com.github.ottx96.system
 
+import com.github.ottx96.Entrypoint
 import com.github.ottx96.Entrypoint.Companion.verbose
 import com.github.ottx96.logging.Colors
 import com.github.ottx96.logging.Styles
 import java.io.File
 import java.util.concurrent.TimeUnit
 
-class ShellCommandExecutor(private val file: File, private val directory: File) {
+class ShellCommandExecutor(private val file: File, private val directory: File, private val action: Entrypoint.Action, private val other: File = file) {
 
     private val err: File = File.createTempFile("DIFFVIEW.", ".TMP")
     private val out: File = File.createTempFile("DIFFVIEW.", ".TMP")
 
     fun execute(): String {
-        if(verbose) {
+        verbose {
             (Styles.ITALIC withColor Colors.WHITE).println("Starting directory: ${directory.absolutePath}")
             (Styles.ITALIC withColor Colors.WHITE).println("Creating temporary file (err): ${err.absolutePath}")
             (Styles.ITALIC withColor Colors.WHITE).println("Creating temporary file (out): ${out.absolutePath}")
@@ -20,13 +21,25 @@ class ShellCommandExecutor(private val file: File, private val directory: File) 
 
         val pb = ProcessBuilder().directory(directory.absoluteFile)
             .redirectError(err).redirectOutput(out)
-            .command("git", "log", "-p", "--follow", file.absolutePath)
 
-        if(verbose) (Styles.ITALIC withColor Colors.WHITE).println("Running system command: ${pb.command().joinToString(separator = " ")}")
+            when(action) {
+                Entrypoint.Action.LOG -> {
+                    pb.command("git", "log", "-p", "--follow", file.absolutePath)
+                }
+                Entrypoint.Action.DIFF -> {
+                    pb.command("git", "diff", "--no-index", file.absolutePath, other.absolutePath)
+                }
+            }
 
-            pb.start().waitFor(5, TimeUnit.SECONDS)
+        verbose {
+            (Styles.ITALIC withColor Colors.WHITE).println("Running system command: ${pb.command().joinToString(separator = " ")}")
+        }
 
-        if(err.length() > 0) throw FileSystemException(err, reason = err.readText())
+        pb.start().waitFor(5, TimeUnit.SECONDS)
+
+        if(err.length() > 0){
+            (Styles.ITALIC withColor Colors.RED).println(err.readText())
+        }
         return out.readText()
     }
 
